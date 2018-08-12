973e0703047a8dce967ba28d4ffc8f602f57ce5b
GustavoRangel15 <32528668+GustavoRangel15@users.noreply.github.com>
2018-08-11 23:15:42 -0300

    Add files via upload

---
+++ POST.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace POSTto
+{
+    public static class POSTto
+    {
+        public static byte[] Post(string uri, NameValueCollection pairs)
+        {
+            byte[] result = null;
+            using (WebClient webClient = new WebClient())
+            {
+                result = webClient.UploadValues(uri, pairs);
+            }
+            return result;
+        }
+    }
+}
